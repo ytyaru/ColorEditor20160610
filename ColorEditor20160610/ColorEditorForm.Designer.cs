7d3699f361b476cca5d3cc44be080c6aef4521eb
ytyaru <ytyaru@outlook.jp>
2016-06-17 07:52:59 +0900

    initial commit

---
+++ ColorEditor20160610/ColorEditorForm.Designer.cs
@@ -0,0 +1,204 @@
+﻿/**
+ * ColorEditor20160610
+ *
+ * These codes are licensed under CC0.
+ * http://creativecommons.org/publicdomain/zero/1.0/deed.ja
+ */
+namespace ColorEditor20160610
+{
+    partial class ColorEditorForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ComponentPanel = new System.Windows.Forms.Panel();
+            this.hScrollBar3 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar2 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.labelBlue = new System.Windows.Forms.Label();
+            this.labelGreen = new System.Windows.Forms.Label();
+            this.labelRed = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.ComponentPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(292, 194);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ComponentPanel
+            // 
+            this.ComponentPanel.Controls.Add(this.hScrollBar3);
+            this.ComponentPanel.Controls.Add(this.hScrollBar2);
+            this.ComponentPanel.Controls.Add(this.hScrollBar1);
+            this.ComponentPanel.Controls.Add(this.numericUpDown3);
+            this.ComponentPanel.Controls.Add(this.numericUpDown2);
+            this.ComponentPanel.Controls.Add(this.numericUpDown1);
+            this.ComponentPanel.Controls.Add(this.labelBlue);
+            this.ComponentPanel.Controls.Add(this.labelGreen);
+            this.ComponentPanel.Controls.Add(this.labelRed);
+            this.ComponentPanel.Location = new System.Drawing.Point(0, 200);
+            this.ComponentPanel.Name = "ComponentPanel";
+            this.ComponentPanel.Size = new System.Drawing.Size(292, 73);
+            this.ComponentPanel.TabIndex = 1;
+            // 
+            // hScrollBar3
+            // 
+            this.hScrollBar3.LargeChange = 1;
+            this.hScrollBar3.Location = new System.Drawing.Point(82, 52);
+            this.hScrollBar3.Maximum = 255;
+            this.hScrollBar3.Name = "hScrollBar3";
+            this.hScrollBar3.Size = new System.Drawing.Size(201, 16);
+            this.hScrollBar3.TabIndex = 8;
+            // 
+            // hScrollBar2
+            // 
+            this.hScrollBar2.LargeChange = 1;
+            this.hScrollBar2.Location = new System.Drawing.Point(82, 27);
+            this.hScrollBar2.Maximum = 255;
+            this.hScrollBar2.Name = "hScrollBar2";
+            this.hScrollBar2.Size = new System.Drawing.Size(201, 16);
+            this.hScrollBar2.TabIndex = 7;
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.LargeChange = 1;
+            this.hScrollBar1.Location = new System.Drawing.Point(82, 4);
+            this.hScrollBar1.Maximum = 255;
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(201, 16);
+            this.hScrollBar1.TabIndex = 6;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(23, 52);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(52, 19);
+            this.numericUpDown3.TabIndex = 5;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(23, 27);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(52, 19);
+            this.numericUpDown2.TabIndex = 4;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(23, 4);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(52, 19);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // labelBlue
+            // 
+            this.labelBlue.AutoSize = true;
+            this.labelBlue.Location = new System.Drawing.Point(3, 54);
+            this.labelBlue.Name = "labelBlue";
+            this.labelBlue.Size = new System.Drawing.Size(13, 12);
+            this.labelBlue.TabIndex = 2;
+            this.labelBlue.Text = "B";
+            // 
+            // labelGreen
+            // 
+            this.labelGreen.AutoSize = true;
+            this.labelGreen.Location = new System.Drawing.Point(4, 29);
+            this.labelGreen.Name = "labelGreen";
+            this.labelGreen.Size = new System.Drawing.Size(13, 12);
+            this.labelGreen.TabIndex = 1;
+            this.labelGreen.Text = "G";
+            // 
+            // labelRed
+            // 
+            this.labelRed.AutoSize = true;
+            this.labelRed.Location = new System.Drawing.Point(4, 4);
+            this.labelRed.Name = "labelRed";
+            this.labelRed.Size = new System.Drawing.Size(13, 12);
+            this.labelRed.TabIndex = 0;
+            this.labelRed.Text = "R";
+            // 
+            // ColorEditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.ComponentPanel);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "ColorEditorForm";
+            this.Text = "ColorEditor";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ComponentPanel.ResumeLayout(false);
+            this.ComponentPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel ComponentPanel;
+        private System.Windows.Forms.HScrollBar hScrollBar3;
+        private System.Windows.Forms.HScrollBar hScrollBar2;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label labelBlue;
+        private System.Windows.Forms.Label labelGreen;
+        private System.Windows.Forms.Label labelRed;
+    }
+}
+
---
+++ ColorEditor20160610/ColorEditorForm.cs
@@ -0,0 +1,138 @@
+﻿/**
+ * ColorEditor20160610
+ *
+ * These codes are licensed under CC0.
+ * http://creativecommons.org/publicdomain/zero/1.0/deed.ja
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ColorEditor20160610
+{
+    public partial class ColorEditorForm : Form
+    {
+        private Color nowColor;
+
+        private System.EventHandler numericUpDown1ValueChangedEventHandler;
+        private System.EventHandler numericUpDown2ValueChangedEventHandler;
+        private System.EventHandler numericUpDown3ValueChangedEventHandler;
+        private System.EventHandler hScrollBar1ValueChangedEventHandler;
+        private System.EventHandler hScrollBar2ValueChangedEventHandler;
+        private System.EventHandler hScrollBar3ValueChangedEventHandler;
+
+        public ColorEditorForm()
+        {
+            InitializeComponent();
+            this.nowColor = Color.FromArgb(0, 0, 0);
+
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+
+            this.numericUpDown1ValueChangedEventHandler = new System.EventHandler(this.numericUpDown1_ValueChanged);
+            this.numericUpDown2ValueChangedEventHandler = new System.EventHandler(this.numericUpDown2_ValueChanged);
+            this.numericUpDown3ValueChangedEventHandler = new System.EventHandler(this.numericUpDown3_ValueChanged);
+
+            this.hScrollBar1ValueChangedEventHandler += new System.EventHandler(this.hScrollBar1_ValueChanged);
+            this.hScrollBar2ValueChangedEventHandler += new System.EventHandler(this.hScrollBar2_ValueChanged);
+            this.hScrollBar3ValueChangedEventHandler += new System.EventHandler(this.hScrollBar3_ValueChanged);
+
+            this.numericUpDown1.ValueChanged += this.numericUpDown1ValueChangedEventHandler;
+            this.numericUpDown2.ValueChanged += this.numericUpDown2ValueChangedEventHandler;
+            this.numericUpDown3.ValueChanged += this.numericUpDown3ValueChangedEventHandler;
+
+            this.hScrollBar1.ValueChanged += this.hScrollBar1ValueChangedEventHandler;
+            this.hScrollBar2.ValueChanged += this.hScrollBar2ValueChangedEventHandler;
+            this.hScrollBar3.ValueChanged += this.hScrollBar3ValueChangedEventHandler;
+
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            this.nowColor = Color.FromArgb(
+                (int)this.numericUpDown1.Value,
+                (int)this.numericUpDown2.Value,
+                (int)this.numericUpDown3.Value);
+            pictureBox1.BackColor = this.nowColor;
+
+            // http://bbs.wankuma.com/index.cgi?mode=al2&namber=7027&KLOG=17
+            this.hScrollBar1.ValueChanged -= this.hScrollBar1ValueChangedEventHandler;
+            this.hScrollBar1.Value = (int)this.numericUpDown1.Value;
+            this.hScrollBar1.ValueChanged += this.hScrollBar1ValueChangedEventHandler;
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            this.nowColor = Color.FromArgb(
+                (int)this.numericUpDown1.Value,
+                (int)this.numericUpDown2.Value,
+                (int)this.numericUpDown3.Value);
+            pictureBox1.BackColor = this.nowColor;
+
+            this.hScrollBar2.ValueChanged -= this.hScrollBar2ValueChangedEventHandler;
+            this.hScrollBar2.Value = (int)this.numericUpDown2.Value;
+            this.hScrollBar2.ValueChanged += this.hScrollBar2ValueChangedEventHandler;
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            this.nowColor = Color.FromArgb(
+                (int)this.numericUpDown1.Value,
+                (int)this.numericUpDown2.Value,
+                (int)this.numericUpDown3.Value);
+            pictureBox1.BackColor = this.nowColor;
+
+            this.hScrollBar3.ValueChanged -= this.hScrollBar3ValueChangedEventHandler;
+            this.hScrollBar3.Value = (int)this.numericUpDown3.Value;
+            this.hScrollBar3.ValueChanged += this.hScrollBar3ValueChangedEventHandler;
+        }
+
+        private void hScrollBar1_ValueChanged(object sender, EventArgs e)
+        {
+            this.nowColor = Color.FromArgb(
+                (int)this.hScrollBar1.Value,
+                (int)this.hScrollBar2.Value,
+                (int)this.hScrollBar3.Value);
+            pictureBox1.BackColor = this.nowColor;
+
+            this.numericUpDown1.ValueChanged -= this.numericUpDown1ValueChangedEventHandler;
+            this.numericUpDown1.Value = this.hScrollBar1.Value;
+            this.numericUpDown1.ValueChanged += this.numericUpDown1ValueChangedEventHandler;
+        }
+
+        private void hScrollBar2_ValueChanged(object sender, EventArgs e)
+        {
+            this.nowColor = Color.FromArgb(
+                (int)this.hScrollBar1.Value,
+                (int)this.hScrollBar2.Value,
+                (int)this.hScrollBar3.Value);
+            pictureBox1.BackColor = this.nowColor;
+
+            this.numericUpDown2.ValueChanged -= this.numericUpDown2ValueChangedEventHandler;
+            this.numericUpDown2.Value = this.hScrollBar2.Value;
+            this.numericUpDown2.ValueChanged += this.numericUpDown2ValueChangedEventHandler;
+        }
+
+        private void hScrollBar3_ValueChanged(object sender, EventArgs e)
+        {
+            this.nowColor = Color.FromArgb(
+                (int)this.hScrollBar1.Value,
+                (int)this.hScrollBar2.Value,
+                (int)this.hScrollBar3.Value);
+            pictureBox1.BackColor = this.nowColor;
+
+            this.numericUpDown3.ValueChanged -= this.numericUpDown3ValueChangedEventHandler;
+            this.numericUpDown3.Value = this.hScrollBar3.Value;
+            this.numericUpDown3.ValueChanged += this.numericUpDown3ValueChangedEventHandler;
+        }
+    }
+}
